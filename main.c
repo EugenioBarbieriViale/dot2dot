39ae16cd4392bdde2a2b6a230800ea05c8d8f642
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-06-25 01:52:56 +0200

    new

--- main.c
+++ main.c
@@ -5,13 +5,35 @@
 #define X 1000
 #define Y 800
 
+# define R 40
+
+int pos[21][3];
+
 void draw_points() {
 	int n = 6;
+	int count = 0;
+
 	for (int j=0; j<6; j++) {
 		for (int i=0; i<n; i++) {
-			DrawCircleLines(300 + 80*i, 200 + 70*j, 25, BLACK);
+			int x = 300 + (2*R+20)*i;
+			int y = 170 + (2*R+10)*j;
+
+			DrawCircle(x, y, R, (Color){255,255,0,90});
+			DrawText(TextFormat("%d", count+1), x - 6, y - 10, 20,WHITE);
+
+			pos[count][0] = count;
+			pos[count][1] = x;
+			pos[count][2] = y;
+
+			count++;
 		}
 		n--;
+	}
+}
+
+void print_arr() {
+	for (int i=0; i<21; i++) {
+		printf("%d. x: %d  y: %d\n", pos[i][0], pos[i][1], pos[i][2]);
 	}
 }
 
@@ -19,24 +41,15 @@ int main() {
 	InitWindow(X,Y, "Erma");
 	SetTargetFPS(60);		
 
-	int px1 = 0;
-	int py1 = 0;
-
 	while (!WindowShouldClose()) {
-		int MX = GetMouseX();
-		int MY = GetMouseY();
-
 		BeginDrawing();
 		ClearBackground(GRAY);
 
-		if (IsMouseButtonPressed(0)) {
-			px1 = MX;
-			py1 = MY;
-		}
+		DrawText("dot2dot game - erase dots in rows!", 50, 10, 50, BLACK);
 
 		draw_points();
-		DrawLine(MX, MY, px1, py1, BLACK);
 
 		EndDrawing();
 	}
+	print_arr();
 }
