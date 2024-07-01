eff4f2ee37a95c06c8e04453eb8728988a15e6b7
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-07-02 01:40:24 +0200

    count erased

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 # define R 40
 
 int pos[21][2];
+int erased[21][2];
+
+int nodes = 0;
+int n_erased;
 
 void draw_points(void) {
 	int n = 6;
@@ -30,11 +34,27 @@ void draw_points(void) {
 	}
 }
 
-int length(int arr[]) {
-	return (int)sizeof(arr) / (int)sizeof(arr[0]);
+bool clicked(int x, int y) {
+	Vector2 mouse_pos = GetMousePosition();
+	Vector2 current_pos = {x,y};
+	if (CheckCollisionPointCircle(mouse_pos, current_pos, R) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) return true;
+	else return false;
+}
+
+void erase() {
+	for (int i=0; i<21; i++) {
+		int x = pos[i][0];
+		int y = pos[i][1];
+		if (clicked(x,y)) {
+			erased[nodes][0] = x;
+			erased[nodes][1] = y;
+			nodes++;
+		}
+	}
 }
 
 void draw_lines(int nodes, int erased[21][2]) {
+	n_erased = 0;
 	for (int i=1; i<nodes; i++)	{
 		Vector2 p1 = {erased[i-1][0], erased[i-1][1]};
 		Vector2 p2 = {erased[i][0], erased[i][1]};
@@ -49,34 +69,18 @@ void draw_lines(int nodes, int erased[21][2]) {
 
 		if (p1.y == p2.y && i%2==1) {
 			DrawLineEx(p1, p2, 5, RED);
+			n_erased += (abs(((p1.x - p2.x)/(2*R+20))) + 1);
 		}
 	}
-}
-
-bool clicked(int x, int y) {
-	Vector2 mouse_pos = GetMousePosition();
-	Vector2 current_pos = {x,y};
-	if (CheckCollisionPointCircle(mouse_pos, current_pos, R) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) return true;
-	else return false;
 }
 
 int main() {
 	InitWindow(X,Y, "dot2dot");
 	SetTargetFPS(60);		
 
-	int nodes = 0;
-	int erased[21][2];
-
-	while (!WindowShouldClose()) {
-		for (int i=0; i<21; i++)	{
-			int x = pos[i][0];
-			int y = pos[i][1];
-			if (clicked(x,y)) {
-				erased[nodes][0] = x;
-				erased[nodes][1] = y;
-				nodes++;
-			}
-		}
+	bool end_game = false;
+	while (!WindowShouldClose() && !end_game) {
+		erase();
 
 		BeginDrawing();
 		ClearBackground(GRAY);
@@ -85,6 +89,9 @@ int main() {
 		draw_points();
 		draw_lines(nodes, erased);
 
+		if (n_erased == 21) end_game = true;
+
 		EndDrawing();
 	}
+	CloseWindow();
 }
