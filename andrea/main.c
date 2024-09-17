a0eb6516cb054a1f9457866caeafede73ff6f442
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-09-17 15:50:03 +0200

    new

--- andrea/main.c
+++ andrea/main.c
@@ -1,170 +1,165 @@
+// TODO:
+//  - add max lines for paths
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <raylib.h>
 
+
 #define X 1000
 #define Y 800
 
-#define N 40
-#define border 100
-#define R 5
+#define N 40 // number of points
+#define R 5  // radius of points
 
-const int size = 20;
-const Rectangle yellow = {border, Y - border - size, size, size};
-const Rectangle blue = {X - border - size, border, size, size};
+#define BORDER 100 // distance from X and Y
+#define RECT_SIZE 20 
+#define LINE_THICKNESS 2
 
-float pos[N][2];
+int yellow_count = 1;
+int blue_count = 1;
 
-float yellow_path[N][2] = {
-	{yellow.x + size / 2, yellow.y + size / 2}
-};
-
-float blue_path[N][2] = {
-	{blue.x + size / 2, blue.y + size / 2}
-};
-
-int yellow_nodes = 1;
-int blue_nodes = 1;
-
-int turn = 0;
-
-float rand_float() {
-	return (float)rand() / (float)RAND_MAX;
-}
-
+float rand_float();
 int choose_turn(int turn);
 
-void init_pos(void);
-void draw_points(void);
+void init_points(Vector2 points[]);
+void draw_points(Vector2 points[]);
 
-bool clicked(float x, float y);
-void create_path(void);
-void draw_lines(void);
+bool clicked(Vector2 current_pos, Vector2 mouse_pos);
+void create_path(Vector2 points[], Vector2 yellow_path[], Vector2 blue_path[], Vector2 mouse_pos, int *turn);
+void draw_lines(Vector2 yellow_path[], Vector2 blue_path[]);
 
-bool check_collision(int counter);
-
-bool game_over(void);
+bool game_over(Vector2 mouse_pos, Rectangle yellow, Rectangle blue);
 
 
 int main() {
-	InitWindow(X,Y, "points");
-	SetTargetFPS(60);		
+    srand(time(NULL));
 
-	srand(time(NULL));
-	init_pos();
+    const Rectangle yellow = {BORDER, Y - BORDER - RECT_SIZE, RECT_SIZE, RECT_SIZE};
+    const Rectangle blue = {X - BORDER - RECT_SIZE, BORDER, RECT_SIZE, RECT_SIZE};
 
-	bool quit = false;
-	while (!WindowShouldClose() && !quit) {
-		create_path();
+    Vector2 points[N];
 
-		BeginDrawing();
-		ClearBackground(GRAY);
+    Vector2 yellow_path[N];
+    yellow_path[0].x = yellow.x + RECT_SIZE / 2;
+    yellow_path[0].y = yellow.y + RECT_SIZE / 2;
 
-		choose_turn(turn);
-		draw_points();
-		draw_lines();
+    Vector2 blue_path[N];
+    blue_path[0].x = blue.x + RECT_SIZE / 2;
+    blue_path[0].y = blue.y + RECT_SIZE / 2;
+
+    int turn = 0;
+    bool quit = false;
+
+    init_points(points);
+
+    InitWindow(X,Y, "points");
+    SetTargetFPS(60);       
 
 
-		DrawRectangleRec(yellow, YELLOW);
-		DrawRectangleRec(blue, BLUE);
+    while (!WindowShouldClose() && !quit) {
+        Vector2 mouse_pos = GetMousePosition();
+        quit = game_over(mouse_pos, yellow, blue);
 
-		DrawText(TextFormat("N: %d", N), X - 90, 10, 30, RED);
-		
-		quit = game_over();
-		EndDrawing();
-	}
-	CloseWindow();
-	return 0;
+        create_path(points, yellow_path, blue_path, mouse_pos, &turn);
+
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        choose_turn(turn);
+        draw_points(points);
+        draw_lines(yellow_path, blue_path);
+
+        DrawRectangleRec(yellow, YELLOW);
+        DrawRectangleRec(blue, BLUE);
+
+        DrawText(TextFormat("N: %d", N), X - 90, 10, 30, RED);
+        
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
 
-void init_pos(void) {
-	for (int i=0; i<N; i++)	{
-		float x = border + rand_float() * (X - border*2);
-		float y = border + rand_float() * (Y - border*2);
-
-		pos[i][0] = x;
-		pos[i][1] = y;
-	}
+float rand_float() {
+    return (float)rand() / (float)RAND_MAX;
 }
 
-void draw_points(void) {
-	for (int i=0; i<N; i++)	{
-		float x = pos[i][0];
-		float y = pos[i][1];
-
-		DrawCircle(x, y, R, BLACK);
-
-		DrawRectangleLines(border, border, X - 2*border, Y - 2*border, RED);
-		/* DrawText(TextFormat("%d", i), x+4, y+4, 30, RED); */
-	}
+void init_points(Vector2 points[]) {
+    for (int i=0; i<N; i++) {
+        points[i].x = BORDER + rand_float() * (X - BORDER * 2);
+        points[i].y = BORDER + rand_float() * (Y - BORDER * 2);
+    }
 }
 
-bool clicked(float x, float y) {
-	Vector2 mouse_pos = GetMousePosition();
-	Vector2 current_pos = {x, y};
-	if (CheckCollisionPointCircle(mouse_pos, current_pos, R) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) return true;
-	else return false;
+void draw_points(Vector2 points[]) {
+    for (int i=0; i<N; i++) {
+        DrawCircle(points[i].x, points[i].y, R, BLACK);
+        DrawRectangleLines(BORDER, BORDER, X - 2 * BORDER, Y - 2 * BORDER, RED);
+        /* DrawText(TextFormat("%d", i), x+4, y+4, 30, RED); */
+    }
 }
 
-bool game_over(void) {
-	Vector2 mouse_pos = GetMousePosition();
+bool clicked(Vector2 current_pos, Vector2 mouse_pos) {
+    if (CheckCollisionPointCircle(mouse_pos, current_pos, R) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        return true;
+    return false;
+}
 
-	if (CheckCollisionPointRec(mouse_pos, yellow) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-		DrawText("BLUE WON", 200,200, 100, BLUE);
-		return true;
-	}
-	else if (CheckCollisionPointRec(mouse_pos, blue) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-		DrawText("YELLOW WON", 200,200, 100, YELLOW);
-		return true;
-	}
-	else return false;
+bool game_over(Vector2 mouse_pos, Rectangle yellow, Rectangle blue) {
+    if (CheckCollisionPointRec(mouse_pos, yellow) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        DrawText("BLUE WON", 200,200, 100, BLUE);
+        return true;
+    }
+    else if (CheckCollisionPointRec(mouse_pos, blue) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        DrawText("YELLOW WON", 200,200, 100, YELLOW);
+        return true;
+    }
+    return false;
 }
 
 int choose_turn(int turn) {
-	if (turn % 2 == 0) {
-		DrawText("YELLOW'S TURN", 10, 10, 30, BLACK);
-		return 0;
-	} else {
-		DrawText("BLUE'S TURN", 10, 10, 30, BLACK);
-		return 1;
-	}
+    if (turn % 2 == 0) {
+        DrawText("YELLOW'S TURN", 10, 10, 30, BLACK);
+        return 0;
+    } else {
+        DrawText("BLUE'S TURN", 10, 10, 30, BLACK);
+        return 1;
+    }
 }
 
-void create_path(void) {
-	for (int i=0; i<N; i++)	{
-		float x = pos[i][0];
-		float y = pos[i][1];
-		if (clicked(x,y)) {
-			if (choose_turn(turn) == 0) {
-				yellow_path[yellow_nodes][0] = x;
-				yellow_path[yellow_nodes][1] = y;
+void create_path(Vector2 points[], Vector2 yellow_path[], Vector2 blue_path[], Vector2 mouse_pos, int *turn) {
+    for (int i=0; i<N; i++) {
 
-				yellow_nodes++;
-			} else {
-				blue_path[blue_nodes][0] = x;
-				blue_path[blue_nodes][1] = y;
+        if (clicked(points[i], mouse_pos)) {
+            if (choose_turn(*turn) == 0) {
+                yellow_path[yellow_count] = points[i];
+                yellow_count++;
+            } 
 
-				blue_nodes++;
-			}
-			if (check_collision(blue_nodes)) printf("Y%d B%d COLLISION\n", yellow_nodes, blue_nodes);
-			turn++;
-		}
-	}
+            else {
+                blue_path[blue_count] = points[i];
+                blue_count++;
+            }
+
+            (*turn)++;
+        }
+    }
 }
 
-void draw_lines(void) {
-	for (int i=1; i<yellow_nodes; i++) {
-		DrawLine(yellow_path[i-1][0], yellow_path[i-1][1], yellow_path[i][0], yellow_path[i][1], YELLOW);
-		/* if (check_collision(yellow_nodes, blue_nodes)) printf("%d COLLISION\n", i); */
-	}
-	for (int i=1; i<blue_nodes; i++) {
-		DrawLine(blue_path[i-1][0], blue_path[i-1][1], blue_path[i][0], blue_path[i][1], BLUE);
-	}
+void draw_lines(Vector2 yellow_path[], Vector2 blue_path[]) {
+    for (int i=1; i<yellow_count; i++) {
+        DrawLineEx(yellow_path[i], yellow_path[i-1], LINE_THICKNESS, YELLOW);
+    }
+    for (int i=1; i<blue_count; i++) {
+        DrawLineEx(blue_path[i], blue_path[i-1], LINE_THICKNESS, BLUE);
+    }
 }
 
 bool is_intersecting(Vector2 start1, Vector2 end1, Vector2 start2, Vector2 end2) {
-	float denominator = ((end1.x - start1.x) * (end2.y - start2.y)) - ((end1.y - start1.y) * (end2.x - start2.x));
+    float denominator = ((end1.x - start1.x) * (end2.y - start2.y)) - ((end1.y - start1.y) * (end2.x - start2.x));
     float numerator1 = ((start1.y - start2.y) * (end2.x - start2.x)) - ((start1.x - start2.x) * (end2.y - start2.y));
     float numerator2 = ((start1.y - start2.y) * (end1.x - start1.x)) - ((start1.x - start2.x) * (end1.y - start1.y));
 
@@ -174,16 +169,4 @@ bool is_intersecting(Vector2 start1, Vector2 end1, Vector2 start2, Vector2 end2)
     float s = numerator2 / denominator;
 
     return (r > 0 && r < 1) && (s > 0 && s < 1);
-}
-
-bool check_collision(int counter) {
-	Vector2 start_yellow = {yellow_path[counter-1][0], yellow_path[counter-1][1]};
-	Vector2 end_yellow = {yellow_path[counter][0], yellow_path[counter][1]};
-
-	Vector2 start_blue = {blue_path[counter-1][0], blue_path[counter-1][1]};
-	Vector2 end_blue = {blue_path[counter][0], blue_path[counter][1]};
-	/* Vector2 start_blue = {blue_path[blue_nodes-1][0], blue_path[blue_nodes-1][1]}; */
-	/* Vector2 end_blue = {blue_path[blue_nodes][0], blue_path[blue_nodes][1]}; */
-	/* printf("%f, %f\n", start_yellow.x, start_yellow.y); */
-	return (is_intersecting(start_yellow, end_yellow, start_blue, end_blue));
 }
