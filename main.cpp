4e1f877d6a49820e62758b1c18c1307766deecd5
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-09-23 21:47:09 +0200

    new

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <raylib.h>
 
-const int X = 1000;
-const int Y = 800;
+#define X 1000
+#define Y 800
 
-const int R = 40;
+#define R 40
 
 std::vector<Vector2> dots;
+std::vector<bool> erased_dots;
 
 std::vector<Vector2> red;
 std::vector<Vector2> blue;
@@ -21,6 +22,7 @@ void init_points() {
             float y = 170 + (2*R+10)*j;
 
             dots.push_back((Vector2){x,y});
+            erased_dots.push_back(false);
         }
         n--;
     }
@@ -45,20 +47,43 @@ void update_turn(int *turn, int *temp) {
     (*temp)++;
 }
 
-void erase(Vector2 mouse_pos, int *turn, int *temp) {
+void update_dot(int temp, int ci, int pi, bool *quit) {
+    if (temp % 2 == 1) {
+        if (ci < pi) {
+            int now = pi;
+            pi = ci;
+            ci = now;
+        }
+
+        for (int j = pi; j <= ci; j++) {
+            if (!erased_dots[j])
+                erased_dots[j] = true;
+            else
+                *quit = true;
+        }
+    }
+}
+
+void erase(Vector2 mouse_pos, int *turn, int *temp, int indexes[], bool *quit) {
     for (int i=0; i<21; i++) {
         if (clicked(mouse_pos, dots[i])) {
 
-            if (*turn % 2 == 0) {
-                red.push_back(dots[i]);
-            } 
-            else {
-                blue.push_back(dots[i]);
-            }
+            indexes[*temp] = i;
 
+            if (*turn % 2 == 0)
+                red.push_back(dots[i]);
+
+            else
+                blue.push_back(dots[i]);
+
+            update_dot(*temp, i, indexes[*temp-1], quit);
             update_turn(turn, temp);
         }
     }
+}
+
+int count_erased(float a, float b) {
+    return (abs(((a - b)/(2*R+20))) + 1);
 }
 
 float shift_lines(float x1, float x2) {
@@ -72,12 +97,14 @@ float shift_lines(float x1, float x2) {
     return shift * 3*R/4;
 }
 
-void draw_red_lines() {
+void draw_red_lines(int *n_erased) {
     for (int i = 0; i < red.size(); i++) {
         Vector2 curr = red[i];
         Vector2 prev = red[i-1];
 
         if (curr.y == prev.y && i % 2 == 1) {
+            *n_erased += count_erased(curr.x, prev.x);
+
             float shift = shift_lines(curr.x, prev.x);
 
             curr.x += shift; 
@@ -88,12 +115,14 @@ void draw_red_lines() {
     }
 }
 
-void draw_blue_lines() {
+void draw_blue_lines(int *n_erased) {
     for (int i = 0; i < blue.size(); i++) {
         Vector2 curr = blue[i];
         Vector2 prev = blue[i-1];
 
         if (curr.y == prev.y && i % 2 == 1) {
+            *n_erased += count_erased(curr.x, prev.x);
+
             float shift = shift_lines(curr.x, prev.x);
 
             curr.x += shift; 
@@ -104,45 +133,23 @@ void draw_blue_lines() {
     }
 }
 
-bool is_intersecting(Vector2 start1, Vector2 end1, Vector2 start2, Vector2 end2) {
-	float denominator = ((end1.x - start1.x) * (end2.y - start2.y)) - ((end1.y - start1.y) * (end2.x - start2.x));
-    float numerator1 = ((start1.y - start2.y) * (end2.x - start2.x)) - ((start1.x - start2.x) * (end2.y - start2.y));
-    float numerator2 = ((start1.y - start2.y) * (end1.x - start1.x)) - ((start1.x - start2.x) * (end1.y - start1.y));
+bool who_won(int n_erased, int turn) {
+    if (n_erased >= 21) {
+        if (turn % 2 == 0)
+            std::cout << "YOU WON" << std::endl;
+        else
+            std::cout << "CHATGPT WON" << std::endl;
 
-    if (denominator == 0)
-        return (numerator1 == 0 && numerator2 == 0);
-    
-    float r = numerator1 / denominator;
-    float s = numerator2 / denominator;
-
-    return (r > 0 && r < 1) && (s > 0 && s < 1);
-}
-
-bool check_line_collsion() {
-    bool coll = false;
-
-    for (int r = 0; r < red.size(); r += 2) {
-        for (int b = 0; b < blue.size(); b += 2) {
-
-            if (blue.size() != 0 && red.size() != 0 && blue.size() % 2 == 0 && red.size() % 2 == 0)
-                // coll = is_intersecting(red[r], red[r+1], blue[b], blue[b+1]);
-                coll = is_intersecting(blue[b], blue[b+1], red[r], red[r+1]);
-
-            if (red[r].x == red[r+1].x || blue[b].x == blue[b+1].x)
-                coll = false;
-        }
+        return true;
     }
-    return coll;
+    return false;
 }
 
-void print_v() {
-    std::cout << "----------------\n";
-    for (auto r : red)
-        std::cout << "red: " << r.x << ", " << r.y << '\n';
-    for (auto b : blue)
-        std::cout << "blue: " << b.x << ", " << b.y << '\n';
+void print_dots() {
+    for (int i=0; i<21; i++) {
+        std::cout << dots[i].x << ", " << dots[i].y << " -> " << erased_dots[i] << std::endl;
+    }
 }
-
 
 int main() {
     InitWindow(X, Y, "dot2dot");
@@ -150,25 +157,32 @@ int main() {
 
     int turn = 0;
     int temp = 0;
+    int indexes[21];
+
+    bool quit = false;
+    bool collision = false;
 
     init_points();
 
-    while (!WindowShouldClose() && !check_line_collsion()) {
-    // while (!WindowShouldClose()) {
+    while (!WindowShouldClose() && !quit && !collision) {
         Vector2 mouse_pos = GetMousePosition();
+        int n_erased = 0;
 
-        // print_v();
-        erase(mouse_pos, &turn, &temp);
+        erase(mouse_pos, &turn, &temp, indexes, &collision);
 
         BeginDrawing();
         ClearBackground(GRAY);
 
         draw_points();
 
-        draw_red_lines();
-        draw_blue_lines();
+        draw_red_lines(&n_erased);
+        draw_blue_lines(&n_erased);
 
         EndDrawing();
+
+        quit = who_won(n_erased, turn);
+
+        // print_dots();
     }
 
     return 0;
