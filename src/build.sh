662deeb41f8ad424c97261b0ab5ce2499b0650e1
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-07-01 17:04:17 +0200

    new

--- src/build.sh
+++ src/build.sh
@@ -2,8 +2,9 @@
 
 set -xe
 
-g++ main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-# g++ human2human.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+# g++ main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+g++ human2human.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+# g++ human2random.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
 ./a.out
 
