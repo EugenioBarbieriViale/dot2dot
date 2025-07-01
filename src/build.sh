bc3ce366cc814313db2bded54596187567cf341c
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-07-01 18:20:06 +0200

    new

--- src/build.sh
+++ src/build.sh
@@ -2,9 +2,9 @@
 
 set -xe
 
-# g++ main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-g++ human2human.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+# g++ human2human.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 # g++ human2random.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+g++ random2random.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
 ./a.out
 
---
+++ src/random2random.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <vector>
+#include <raylib.h>
+
+#define X 1000
+#define Y 800
+
+#define R 40
+
+void init_points(std::vector<Vector2>& dots, std::vector<bool>& erased_dots) {
+    int n = 6;
+
+    for (int j=0; j<6; j++) {
+        for (int i=0; i<n; i++) {
+            float x = 300 + (2*R+20)*i;
+            float y = 170 + (2*R+10)*j;
+
+            dots.push_back((Vector2){x,y});
+            erased_dots.push_back(false);
+        }
+        n--;
+    }
+}
+
+void draw_points(const std::vector<Vector2>& dots, Color c) {
+    for (int i=0; i<21; i++) {
+        DrawCircle(dots[i].x, dots[i].y, R, c);
+        DrawText(TextFormat("%d", i), dots[i].x - 6, dots[i].y - 10, 20, WHITE);
+    }
+}
+
+bool clicked(Vector2 mouse_pos, Vector2 current_pos) {
+    if (CheckCollisionPointCircle(mouse_pos, current_pos, R) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        return true;
+    return false;
+}
+
+void update_dot(std::vector<bool>& erased_dots, int ci, int pi, bool& coll) {
+    if (ci < pi) {
+        int now = pi;
+        pi = ci;
+        ci = now;
+    }
+
+    for (int j = pi; j <= ci; j++) {
+        if (!erased_dots[j])
+            erased_dots[j] = true;
+        else
+            coll = true;
+    }
+}
+
+int count_erased(const std::vector<bool>& erased_dots) {
+    int count = 0;
+    for (auto x:erased_dots) {
+        if (x) {
+            count++;
+        }
+    }
+    return count;
+}
+
+float shift_lines(float x1, float x2) {
+    float shift = x1 - x2;
+
+    if (shift != 0)
+        shift /= abs(shift);
+    else
+        return 3*R/4;
+
+    return shift * 3*R/4;
+}
+
+void draw_red_lines(const std::vector<Vector2>& red) {
+    for (int i = 1; i < red.size(); i++) {
+        Vector2 curr = red[i];
+        Vector2 prev = red[i-1];
+
+        if (curr.y == prev.y && i % 2 == 1) {
+            float shift = shift_lines(curr.x, prev.x);
+
+            curr.x += shift; 
+            prev.x -= shift; 
+
+            DrawLineEx(curr, prev, 5, RED);
+        }
+    }
+}
+
+void draw_blue_lines(const std::vector<Vector2>& blue) {
+    for (int i = 1; i < blue.size(); i++) {
+        Vector2 curr = blue[i];
+        Vector2 prev = blue[i-1];
+
+        if (curr.y == prev.y && i % 2 == 1) {
+            float shift = shift_lines(curr.x, prev.x);
+
+            curr.x += shift; 
+            prev.x -= shift; 
+
+            DrawLineEx(curr, prev, 5, BLUE);
+        }
+    }
+}
+
+bool who_won(int n_erased, int turn) {
+    if (n_erased >= 21) {
+        if (turn % 2 == 0)
+            std::cout << "RED WON" << std::endl;
+        else
+            std::cout << "BLUE WON" << std::endl;
+
+        return true;
+    }
+    return false;
+}
+
+std::vector<int> generate_rnd(const std::vector<bool>& erased_dots) {
+    std::vector<int> idxs;
+
+    int i1, i2;
+    int c = 0;
+    bool go = false;
+
+    do {
+        go = false;
+        int col = std::rand() % 6;
+
+        i1 = (std::rand() % (6 - col));
+        i2 = (std::rand() % (6 - col));
+
+        for (int i=0; i<col; i++) {
+            i1 += (6 - i);
+            i2 += (6 - i);
+        }
+
+        if (i1 > i2) {
+            int now = i2;
+            i2 = i1;
+            i1 = now;
+        }
+
+        for (int j = i1; j <= i2; j++) {
+            if (erased_dots[j])
+                go = true;
+        }
+
+        c++;
+        // std::cout << c << "\n";
+
+    } while ((erased_dots[i1] || erased_dots[i2] || go) && c <= 1000);
+
+    idxs.push_back(i1);
+    idxs.push_back(i2);
+
+    return idxs;
+}
+
+
+int main() {
+    std::srand(time(0));
+
+    InitWindow(X, Y, "dot2dot");
+    SetTargetFPS(30);
+
+    std::vector<Vector2> dots;
+    std::vector<bool> erased_dots;
+
+    std::vector<Vector2> red;
+    std::vector<Vector2> blue;
+
+    Color c = BEIGE;
+    Color bg_c = RAYWHITE;
+
+    int turn = 0;
+    bool collision = false;
+
+    init_points(dots, erased_dots);
+
+    while (!WindowShouldClose()) {
+        int n_erased = count_erased(erased_dots);
+
+        Vector2 mouse_pos = GetMousePosition();
+
+        if (n_erased != 0) {
+            WaitTime(1);
+        }
+
+        if (n_erased < 21) {
+            std::vector<int> rnd1 = generate_rnd(erased_dots);
+            std::vector<int> rnd2 = generate_rnd(erased_dots);
+
+            if (turn % 2 == 0) {
+                red.push_back(dots[rnd1[0]]);
+                red.push_back(dots[rnd1[1]]);
+
+                update_dot(erased_dots, rnd1[0], rnd1[1], collision);
+            } 
+
+            else {
+                blue.push_back(dots[rnd2[0]]);
+                blue.push_back(dots[rnd2[1]]);
+
+                update_dot(erased_dots, rnd2[0], rnd2[1], collision);
+            }
+
+            turn++;
+        }
+
+        BeginDrawing();
+        ClearBackground(bg_c);
+
+        if (n_erased >= 21) {
+            WaitTime(1);
+            DrawRectangle(0, 0, X, Y, bg_c);
+
+            if (turn % 2 == 0)
+                DrawText("RED WON", 175, Y/2-60, 150, RED);
+            else
+                DrawText("BLUE WON", 120, Y/2-60, 150, BLUE);
+        }
+
+        else if (collision) {
+            DrawRectangle(0, 0, X, Y, bg_c);
+            DrawText("COLLISION", 110, Y/2-60, 150, c);
+        }
+
+        else {
+            draw_points(dots, c);
+
+            draw_blue_lines(blue);
+            draw_red_lines(red);
+        }
+
+
+        EndDrawing();
+    }
+
+    return 0;
+}
