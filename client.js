b8d2814a738188871a0b0fb1d501932ceb33b227
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-09-06 21:59:47 +0200

    new

--- client.js
+++ client.js
@@ -1,23 +1,65 @@
-// replace with real url
-const ws = new WebSocket('ws://localhost:8080');
+// const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket("https://eugeniobarbieriviale.github.io/dot2dot-telegram/");
 
 canvas = document.getElementById("game");
 canvas.style.background = "white";
 
+ctx = canvas.getContext("2d");
+ctx.font = "30px Helvetica";
 
-const game = new Game(canvas, 6, 20, 375, 375);
-game.run();
+const r = 20;
+const w = 375;
+const h = 375;
 
-const gameState = game.dots;
+let myId;
+let dots = [];
 
-canvas.addEventListener("mousedown", () => {
-    if (game.temp % 2 == 0) {
-        ws.send(JSON.stringify({
-            type: "gameState",
-            data: gameState,
-        }));
+
+function isToSend(dots, x, y) {
+    for (let i=0; i<dots.length; i++) {
+        let dot = dots[i];
+
+        const dx = x - dot[0];
+        const dy = y - dot[1];
+
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        if (dist < r) {
+            return dot[3];
+        }
     }
-});
+
+    return null;
+}
+
+function displayGame(dots, end) {
+    ctx.clearRect(0, 0, w, h);
+
+    for (let i=0; i<dots.length; i++) {
+        let dot = dots[i];
+
+        if (!end) {
+            if (!dot[2] && !dot[4]) {
+                ctx.fillStyle = "black";
+            }
+            else if (dot[2]) {
+                ctx.fillStyle = "white";
+            }
+            else {
+                ctx.fillStyle = "grey";
+            }
+
+            ctx.beginPath();
+            ctx.arc(dot[0], dot[1], r, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+}
+
+function displayText(playerId) {
+    ctx.fillText(playerId, 130, 38);
+    ctx.fillText("dot2dot", 260, 350);
+}
 
 ws.onopen = function(event) {
     console.log("You are connected to the server");
@@ -25,9 +67,43 @@ ws.onopen = function(event) {
 
 ws.onmessage = function(event) {
     const data = JSON.parse(event.data);
-    console.log(data);
-    // game.dots = data;
-}
+
+    if (data.type == "init") {
+        myId = data.playerId; 
+        alert("You are " + myId);
+
+        dots = data.gameState;
+    }
+
+    else if (data.type == "updated" && data.gameState != undefined) {
+        dots = data.gameState;
+    }
+
+    displayGame(dots, false, r);
+
+    if (data.turn != undefined) {
+        displayText(data.turn);
+    } else {
+        displayText(myId);
+    }
+};
+
+canvas.addEventListener("mousedown", (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.pageX - rect.left;
+    const mouseY = event.pageY - rect.top;
+
+    let index = isToSend(dots, mouseX, mouseY, r);
+
+    if (index != null) {
+        ws.send(JSON.stringify({
+            type: "update",
+            playerId: myId,
+            dotIndex: index,
+            gameState: dots,
+        }));
+    }
+});
 
 ws.onclose = function(event) {
     console.log("You are disconnected from the server");
--- server.js
+++ server.js
@@ -1,29 +1,128 @@
 const port = 8080;
 
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port });
+const server = new WebSocket.Server({ port });
+
+const n = 6;
+const r = 20;
+
+let players = [];
+let playerCount = 0;
+
+function initDots() {
+    let ans = [];
+
+    let c = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < (n - i); j++) {
+
+            let x = 40 + (2 * r + 20) * j;
+            let y = 85 + (2 * r + 10) * i;
+            
+            ans.push([x, y, false, c, false]);
+            c++;
+        }
+    }
+
+    return ans;
+}
+
+function updateGameState(pairIndex, currentGameState) {
+    if (pairIndex.length != 2)  {
+        return undefined;
+    }
+
+    let i1 = pairIndex[0];
+    let i2 = pairIndex[1];
+
+    if (currentGameState[i1][1] != currentGameState[i2][1]) {
+        return currentGameState;
+    }
+
+    let newGameState = currentGameState;
+
+    if (i1 < i2) {
+        for (let i = i1; i <= i2; i++) {
+            if (newGameState[i][2]) break;
+            newGameState[i][2] = true;
+        }
+    }
+
+    else {
+        for (let i = i1; i >= i2; i--) {
+            if (newGameState[i][2]) break;
+            newGameState[i][2] = true;
+        }
+    }
+
+    return newGameState;
+}
 
 function broadcast(data) {
-    wss.clients.forEach(client => {
+    server.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify(data));
         }
     });
 }
 
-wss.on('connection', function connection(ws) {
-    // broadcast({
-    //     type: "init",
-    //     data: "connection established",
-    // });
+server.on('connection', function connection(ws) {
+    playerCount++;
+
+    let playerId = "player" + playerCount;
+    console.log(playerId + " has connected!");
+
+    players.push(playerId);
+
+    if (players.length >= 2) {
+        ws.send(JSON.stringify({
+            type: "overflow",
+            message: "Two users are already playing!",
+        }));
+    }
     
+    let dots = initDots();
+    let pairIndex = [];
+    let turn = 0;
+
+    broadcast({
+        type: 'init',
+        playerId: playerId,
+        gameState: dots,
+    });
+
+    // console.log(players[turn % 2]);
+    console.log(players);
+
     ws.onmessage = function(event) {
         const data = JSON.parse(event.data);
-        console.log(data);
 
-        broadcast({
-            type: "update",
-            data: data,
-        });
+        if (data.type == "update" && data.playerId == players[turn % 2]) {
+        // if (data.type == "update") {
+            pairIndex.push(data.dotIndex);
+
+            let newGameState = updateGameState(pairIndex, data.gameState);
+
+            if (newGameState != undefined) {
+                broadcast({
+                    type: "updated",
+                    turn: players[turn % 2],
+                    gameState: newGameState,
+                });
+
+                pairIndex = [];
+                turn++;
+            }
+        }
     }
+
+    ws.on('close', function() {
+        console.log(`${playerId} has disconnected`);
+        const playerIndex = playerId.length - 1;
+
+        players.splice(playerIndex, 1);
+        playerCount--;
+
+        console.log(players);
+    });
 });
